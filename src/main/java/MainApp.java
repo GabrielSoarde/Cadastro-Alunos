5dcafbd94b38a1de3cffcafd6e300fa0f11d2706
Soarde <soardegabriel@gmail.com>
2025-08-02 15:10:04 -0300

    pacote helper removido

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,4 +1,3 @@
-import helper.BancoHelper;
 import ui.dialog.JanelaPrincipal;
 
 import javax.swing.*;
@@ -6,6 +5,5 @@ import javax.swing.*;
 public class MainApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new JanelaPrincipal().setVisible(true));
-        new BancoHelper().criarTodasTabelas();
     }
 }
